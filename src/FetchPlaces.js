b25d0e7d0aeb45f722a95dae7a5624eaa29e46af
gos0808 <124739104+gos0808@users.noreply.github.com>
2025-01-13 21:24:25 -0500

    Update FetchPlaces.js

--- src/FetchPlaces.js
+++ src/FetchPlaces.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const getPlaces = (setPlaces) => {
-axios.get('https://places-to-visit-backend-2.onrender.com')
+// axios.get('https://places-to-visit-backend-2.onrender.com')
+    axios.get('https://130.61.33.105:8001/')
     .then(({ data }) => {
         setPlaces(data)
     })
@@ -10,7 +11,10 @@ axios.get('https://places-to-visit-backend-2.onrender.com')
     });
 };
 
-export const savePlace = ({name, province, description, image, setName, setProvince, setDescription, setImage, setPlaces}) => {  axios.post('https://places-to-visit-backend-2.onrender.com/savePlace', {name, province, description, image})
+export const savePlace = ({name, province, description, image, setName, setProvince, setDescription, setImage, setPlaces}) => {  
+    // axios.post('https://places-to-visit-backend-2.onrender.com/savePlace', 
+    axios.post('https://130.61.33.105:8001/savePlace',
+               {name, province, description, image})
     .then(() => {
 
         setName('');
@@ -26,7 +30,9 @@ export const savePlace = ({name, province, description, image, setName, setProvi
 }
 
 export const editPlace = ({id, name, province, description, image, setName, setProvince, setDescription, setImage, setEdititng, setPlaces}) => {
-axios.put(`https://places-to-visit-backend-2.onrender.com/editPlace`, {_id: id, name, province, description, image})
+// axios.put(`https://places-to-visit-backend-2.onrender.com/editPlace`, 
+axios.put(`https://130.61.33.105:8001/editPlace`, 
+          {_id: id, name, province, description, image})
     .then(({ data }) => {
 
         setName('');
@@ -44,7 +50,8 @@ axios.put(`https://places-to-visit-backend-2.onrender.com/editPlace`, {_id: id, 
 
 export const deletePlaces = ({id, setPlaces}) => {
     const _id = id; 
-    axios.delete(`https://places-to-visit-backend-2.onrender.com/deletePlace/${_id}`)
+    // axios.delete(`https://places-to-visit-backend-2.onrender.com/deletePlace/${_id}`)
+    axios.delete(`http://130.61.33.105:8001/deletePlace/${_id}`)
         .then(({ data }) => {
             getPlaces(setPlaces)
         })
